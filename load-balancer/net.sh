b68c7d4f7ebecebff469558f1b5202df309562da
Achess <jealtzo26@gmail.com>
2025-10-18 14:47:04 -0600

    feat: Add load balancer vlans config

--- load-balancer/net.sh
+++ load-balancer/net.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
-IFACE="enx00e04c3600fa"    # Interfaz física conectada al firewall
-VLAN1_ID=10
-VLAN2_ID=20
-IP_ISP1="10.10.10.3/24"
-IP_ISP2="10.10.20.3/24"
-GW_ISP1="10.10.10.1"
-GW_ISP2="10.10.20.1"
+IFACE="enx00e04c3600fa"    # Interface physical connected to firewall
 
-# Nombres cortos para las VLANs
-VLAN1_NAME="wan10"
-VLAN2_NAME="wan20"
+# VLAN IDs must match those sent from the ISP simulator
+# These are the VLANs that are now passing transparently THROUGH the firewall
+VLAN1_ID=70
+VLAN2_ID=80
+
+# IPs now come from the ISP networks
+IP_ISP1="192.168.70.3/24" # Example IP on ISP1's network
+IP_ISP2="192.168.80.3/24" # Example IP on ISP2's network
+
+# Gateways are now the ISP gateways themselves
+GW_ISP1="192.168.70.1"
+GW_ISP2="192.168.80.1"
+
+# Nombres for VLANs can be whatever you want
+VLAN1_NAME="wan70"
+VLAN2_NAME="wan80"
 
 create_vlans() {
   echo "[+] Creando VLANs..."
