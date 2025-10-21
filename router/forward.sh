ee5b6e03a9d33f3dff8f86f1a6f34c60c70bc265
Javier Oswaldo Sacor Quijivix <javoswsq@gmail.com>
2025-10-20 23:19:56 -0600

    some scripts added

---
+++ router/forward.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Activando IP forwarding"
+
+# Agregar la configuración al archivo sysctl.conf
+echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
+
+# Aplicar los cambios inmediatamente
+sysctl -p
+
+# Verificar que se activó correctamente
+echo "Verificación:"
+echo "en sysctl: $(sysctl net.ipv4.ip_forward)"
+echo "en /proc/sys/net/ipv4/ip_forward: $(cat /proc/sys/net/ipv4/ip_forward)"
